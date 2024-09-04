0e159f32dd01d060f4a5599bc3c99e717bf1ac84
RAM <rahul.munde@indiumsoft.com>
2024-09-04 17:48:34 +0530

    API fetching

--- js/index.js
+++ js/index.js
@@ -1,3 +1,23 @@
+const url = "https://api.github.com/users/rahul-munde/repos";
+
+fetch(url)
+  .then((response) => response.json())
+  .then((repos) => {
+    const projectSection = document.getElementById("projectSection");
+    const projectList = projectSection.querySelector("ul");
+    let count = 0;
+    repos.forEach((repo) => {
+      if (count > 10) {
+        return;
+      }
+      const project = document.createElement("li");
+      project.innerText = repo.name;
+      projectList.append(project);
+      count += 1;
+    });
+  })
+  .catch((e) => console.log(e));
+
 function displayTime() {
   const clock = document.getElementById("clock");
   const time = new Date();
